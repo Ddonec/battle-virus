68e4a9b98608a6857fb8ea790a771571a98153c6
Anton <137609245+Ddonec@users.noreply.github.com>
2024-04-16 15:59:11 +0300

    feat: miltify of end points create functional + add function for chose a random point

--- src/components/Game/GameEnd.jsx
+++ src/components/Game/GameEnd.jsx
@@ -1,27 +1,28 @@
 import React, { useContext } from "react";
 import { GameContext } from "../../context";
 import { generateBoard } from "../../utils/Common";
+import { getRandomFromArray } from "../../utils/Common";
 
 function GameEnd({ setBoard1, setBoard2 }) {
+   const stringArr = ["строка 1", "строка 2", "строка 3", "строка 4"];
+   const stringArrWin = ["строка победы 1", "строка победы 2", "строка победы 3", "строка победы 4"];
 
-    const { winner } = useContext(GameContext);
+   const { winner } = useContext(GameContext);
 
-    function resetGame() {
-        setBoard1([]);
-        setBoard2(generateBoard());
-    }
-    return (
-        <>
-            {
-                winner !== null && (
-                    <div onClick={resetGame} className="game-end-container">
-                        <h3 className="game-end-header shadowed">{winner === 0 ? "You lost" : "You won"}</h3>
-                        <h5 className="shadowed-tiny">Click to start a new game!</h5>
-                    </div>
-                )
-            }
-        </>
-    );
+   function resetGame() {
+      setBoard1([]);
+      setBoard2(generateBoard());
+   }
+   return (
+      <>
+         {winner !== null && (
+            <div onClick={resetGame} className="game-end-container">
+               <h3 className="game-end-header shadowed">{winner === 0 ? "You lost " + getRandomFromArray(stringArr) : "You won " + getRandomFromArray(stringArrWin)}</h3>
+               <h5 className="shadowed-tiny">Click to start a new game!</h5>
+            </div>
+         )}
+      </>
+   );
 }
 
 export default GameEnd;
