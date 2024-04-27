2cdc9d11ee7254383144d268ba6ddfdf6c8f97d2
Anton <137609245+Ddonec@users.noreply.github.com>
2024-04-27 16:00:52 +0300

    feat: rules modal will update + new design

--- src/components/Game/StartModal.jsx
+++ src/components/Game/StartModal.jsx
@@ -5,17 +5,15 @@ function StartModal({ clickHide }) {
       <>
          <div className="game-start-container">
             <div className="logo"></div>
-            <div className="blue-container">
-               <div className="hero-img"></div>
+            <div className="blue-container start-blue">
                <div className="justify-left">
-                  <div className="border-w-div">игра</div>
-                  <h3 className="welcome-title ">Морской бой</h3>
-                  <p className="welcome-subtitle">Ученые против микробов</p>
-               </div>
-            </div>
-            <div className="btnS-area">
-               <div onClick={clickHide} className="close-btn">
-                  Приступить
+                  <h3 className="start-title welcome-title">МИССИЯ:</h3>
+                  <p className="welcome-subtitle start-sub">найти и обезвредить</p>
+                  <div className="btnS-area">
+                     <div onClick={clickHide} className="close-btn">
+                        Приступить
+                     </div>
+                  </div>
                </div>
             </div>
          </div>
--- src/components/Game/WelcomeHello.jsx
+++ src/components/Game/WelcomeHello.jsx
@@ -5,20 +5,29 @@ function WelcomeHi({ clickCB, clickR }) {
       <>
          <div className="game-start-container">
             <div className="logo"></div>
-            <div className="blue-container">
-               <div className="hero-img"></div>
-               <div className="justify-left">
-                  <div className="border-w-div">игра</div>
-                  <h3 className="welcome-title ">Морской бой</h3>
-                  <p className="welcome-subtitle">Ученые против микробов</p>
-               </div>
-            </div>
-            <div className="btnS-area">
-               <div onClick={clickR} className="rule-btn">
-                  Правила
-               </div>
-               <div onClick={clickCB} className="close-btn">
-                  Старт
+            <div className="welcome-bg-container">
+               <div className="welocme-white-container">
+                  <h3 className="welcome-title blue-text">
+                     Добро пожаловать <br /> в команду!
+                  </h3>
+                  <div className="welcome-subtitle black-text instruction">
+                     <p>Присоединяйтесь к миссии «Промомед». Давайте вместе делать людей здоровыми, красивыми и счастливыми.</p>
+                     <p>
+                        Ежедневно человечество атакуют сотни инфекций. В борьбе за каждую жизнь мы раз за разом продолжаем отбивать их коварные нападки. Сегодня перед нами стоит сложная задача: найти
+                        и обезвредить вирусы, бактерии, грибы и паразитов на игровом поле, чтобы минимизировать угрозу здоровью и жизни человечества.
+                     </p>
+                     <p>Чтобы миссия прошла успешно, правильно экипируйтесь и эффективно используйте лекарственные средства.</p>
+                     <p className="lets-ready">Готовы? Тогда начинаем!</p>
+                  </div>
+
+                  <div className="btnS-area">
+                     <div onClick={clickR} className="rule-btn">
+                        Правила
+                     </div>
+                     <div onClick={clickCB} className="close-btn">
+                        Старт
+                     </div>
+                  </div>
                </div>
             </div>
          </div>
