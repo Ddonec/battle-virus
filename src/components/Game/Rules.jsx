ebcbd399965c0fc4f073868b545c99222ed9e6a5
Anton <137609245+Ddonec@users.noreply.github.com>
2024-05-01 13:43:42 +0300

    feat: update modal wiev + logick, font size, background, correct for client

--- src/components/Game/Rules.jsx
+++ src/components/Game/Rules.jsx
@@ -3,41 +3,41 @@ import React from "react";
 function Rules({ clickCB }) {
    return (
       <>
-         <div className="game-start-container">
-            <div className="logo"></div>
-            <div className="rules-bg-container">
-               <div className="welocme-white-container rules-white">
-                  <h3 className="welcome-title blue-text">Правила игры </h3>
-                  <p className="lets-ready rules-1-b">«Миссия: найти и обезвредить»</p>
-                  <div className="welcome-subtitle black-text rules">
-                     <p>
-                        <div className="number-list">1</div>Ваши помощники в игре — лекарственные средства. Расположите их в своей зоне самостоятельно или воспользуйтесь кнопкой «Случайная
-                        расстановка». Вы можете менять положение лекарственных средств бесконечное количество раз .
-                     </p>
-                     <p>
-                        <div className="number-list">2</div>На стороне оппонента — бактерии, грибы, вирусы и паразиты.
-                     </p>
-                     <p>
-                        <div className="number-list">3</div>Ваша задача — обезвредить все виды инфекций до того, как они уничтожат Ваш лекарственный арсенал.
-                     </p>
-                     <p>
-                        <div className="number-list">4</div>Выберите курсором место в зоне соперника, где, по-Вашему, он скрывает своих болезнетворных агентов
-                     </p>
-                     <p>
-                        <div className="number-list">5</div>Ваше попадание в цель оставляет следующий ход за Вами. Ваш промах дает возможность ответить Вашему противнику .
-                     </p>
-                     <p>
-                        <div className="number-list">6</div>Победителем считается тот, кто первым найдет и обезвредит весь потенциал противника!
-                     </p>
-                  </div>
-                  <p className="lets-ready">Удачи!</p>
+         <div className="game-start-container rules-update">
+            {/* <div className="logo"></div> */}
+            {/* <div className="rules-bg-container"> */}
+            <div className="welocme-white-container rules-white">
+               <h3 className="welcome-title blue-text">Правила игры </h3>
+               <p className="lets-ready rules-1-b">«Миссия: найти и обезвредить»</p>
+               <div className="welcome-subtitle black-text rules">
+                  <p>
+                     <div className="number-list">1</div>Ваши помощники в игре — лекарственные средства. Расположите их в своей зоне самостоятельно или воспользуйтесь кнопкой «Случайная расстановка».
+                     Вы можете менять положение лекарственных средств бесконечное количество раз .
+                  </p>
+                  <p>
+                     <div className="number-list">2</div>На стороне оппонента — бактерии, грибы, вирусы и паразиты.
+                  </p>
+                  <p>
+                     <div className="number-list">3</div>Ваша задача — обезвредить все виды инфекций до того, как они уничтожат Ваш лекарственный арсенал.
+                  </p>
+                  <p>
+                     <div className="number-list">4</div>Выберите курсором место в зоне соперника, где, по-Вашему, он скрывает своих болезнетворных агентов
+                  </p>
+                  <p>
+                     <div className="number-list">5</div>Ваше попадание в цель оставляет следующий ход за Вами. Ваш промах дает возможность ответить Вашему противнику .
+                  </p>
+                  <p>
+                     <div className="number-list">6</div>Победителем считается тот, кто первым найдет и обезвредит весь потенциал противника! Удачи!
+                  </p>
+               </div>
+               {/* <p className="lets-ready">Удачи!</p> */}
 
-                  <div className="btnS-area">
-                     <div onClick={clickCB} className="close-btn">
-                        Старт
-                     </div>
+               <div className="btnS-area">
+                  <div onClick={clickCB} className="close-btn">
+                     Старт
                   </div>
                </div>
+               {/* </div> */}
             </div>
          </div>
       </>
--- src/components/Game/WelcomeHello.jsx
+++ src/components/Game/WelcomeHello.jsx
@@ -3,15 +3,17 @@ import React from "react";
 function WelcomeHi({ clickCB, clickR }) {
    return (
       <>
-         <div className="game-start-container">
-            <div className="logo"></div>
+         <div className="game-start-container start-blure-bg">
+            {/* <div className="logo"></div> */}
             <div className="welcome-bg-container">
+               {/* <div className="heri-hi-absolute"></div> */}
+               {/* <div className="colb-absolute"></div> */}
                <div className="welocme-white-container">
-                  <h3 className="welcome-title blue-text">
+                  <h3 className="welcome-title blue-text text-caps">
                      Добро пожаловать <br /> в команду!
                   </h3>
                   <div className="welcome-subtitle black-text instruction">
-                     <p>Присоединяйтесь к миссии «Промомед». Давайте вместе делать людей здоровыми, красивыми и счастливыми.</p>
+                     <p className="lets-ready">Присоединяйтесь к миссии «Промомед». Давайте вместе делать людей здоровыми, красивыми и счастливыми.</p>
                      <p>
                         Ежедневно человечество атакуют сотни инфекций. В борьбе за каждую жизнь мы раз за разом продолжаем отбивать их коварные нападки. Сегодня перед нами стоит сложная задача: найти
                         и обезвредить вирусы, бактерии, грибы и паразитов на игровом поле, чтобы минимизировать угрозу здоровью и жизни человечества.
