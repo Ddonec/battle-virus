d32c22beaef88f8f0fc3a387076d262aa83ab226
Anton <137609245+Ddonec@users.noreply.github.com>
2024-05-02 14:01:55 +0300

    feat: final game end containers update

--- src/components/Game/GameEnd.jsx
+++ src/components/Game/GameEnd.jsx
@@ -1,12 +1,12 @@
 import React, { useContext } from "react";
 import { GameContext } from "../../context";
 import { generateBoard } from "../../utils/Common";
-import { getRandomFromArray } from "../../utils/Common";
+import { getRandomFromArray, getRandomNumber } from "../../utils/Common";
 
 function GameEnd({ setBoard1, setBoard2 }) {
    const stringTitleLostArr = ["Почти получилось! ", "Противник хорошо маскируется", "Вы раскрыты, миссия провалена!"];
-   const stringArr = ["Готовьте новых агентов и продолжайте миссию!", "Используйте план Б и повторите попытку!", "Не сдавайтесь, попробуйте еще раз! "];
-   const stringArrWin = [`Все инфекции найдены и обезврежены! <br/>Мы гордимся, что Вы часть нашей команды!`];
+   const stringArr = ["Готовьте новых агентов", "Используйте план Б", "Не сдавайтесь,"];
+   const stringArrNext = [" и продолжайте миссию!", " и повторите попытку!", "попробуйте еще раз! "];
    const imgArr = ["hero-loser-1", "hero-loser-2", "hero-loser-3"];
 
    const { winner } = useContext(GameContext);
@@ -15,6 +15,8 @@ function GameEnd({ setBoard1, setBoard2 }) {
       setBoard1([]);
       setBoard2(generateBoard());
    }
+   let i = getRandomNumber();
+   console.log(i);
    return (
       <>
          {winner !== null && (
@@ -22,13 +24,17 @@ function GameEnd({ setBoard1, setBoard2 }) {
                <div className="game-end-logo"></div>
                <div className="game-end-box">
                   <div className="white-cont">
-                     <div className={winner === 0 ? getRandomFromArray(imgArr) : "hero-img-winner"}></div>
+                     <div className={winner === 0 ? imgArr[i] : "hero-img-winner"}></div>
                      <div>
                         <h3 className="game-end-header">
                            {winner === 0 ? (
                               <>
-                                 {getRandomFromArray(stringTitleLostArr)}
-                                 <p className="winner-subtitle">{getRandomFromArray(stringArr)}</p>
+                                 {stringTitleLostArr[i]}
+                                 <p className="winner-subtitle">
+                                    {stringArr[i]}
+                                    <br />
+                                    {stringArrNext[i]}{" "}
+                                 </p>
                               </>
                            ) : (
                               <>
@@ -37,7 +43,7 @@ function GameEnd({ setBoard1, setBoard2 }) {
                                     Все инфекции найдены и обезврежены! <br />
                                     Мы гордимся, что Вы часть нашей команды!
                                  </p>
-                              </>
+                              </> 
                            )}
                         </h3>
                         <div onClick={resetGame} className="go-next-btn">
