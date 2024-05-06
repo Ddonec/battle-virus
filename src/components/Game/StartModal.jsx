24815025f7551475ba63b3b4aaea50fa1b9da90c
Anton <137609245+Ddonec@users.noreply.github.com>
2024-05-06 08:30:53 +0300

    feat: ml 0 first page

--- src/components/Game/StartModal.jsx
+++ src/components/Game/StartModal.jsx
@@ -7,8 +7,10 @@ function StartModal({ clickHide }) {
             <div className="logo"></div>
             <div className="blue-container start-blue">
                <div className="justify-left">
-                  <h3 className="start-title welcome-title">МИССИЯ:</h3>
-                  <p className="welcome-subtitle start-sub text ml-15">найти и обезвредить</p>
+                  <div className="first-modal-text-line">
+                     <h3 className="start-title welcome-title">МИССИЯ:</h3>
+                     <p className="welcome-subtitle start-sub text ml-15">найти и обезвредить</p>
+                  </div>
                   <div className="btnS-area">
                      <div onClick={clickHide} className="close-btn">
                         Приступить
